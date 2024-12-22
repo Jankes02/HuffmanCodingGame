d0293792b0a26d48fadaec1102c4d12e0289bc51
Rafał Jankowski <r.jankowski@nexpertis.pl>
2024-12-22 22:07:00 +0100

    added button actions and help tab

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
